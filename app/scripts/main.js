e74919a79dc59593f45092fd23057769f8327f7a
Chris Vasquez <christv@aweber.com>
2015-12-07 21:35:38 -0500

    Setup leaderboard screens

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var   createEntry = $('#createEntry'),
+      newEntry = $('#newEntry'),
+      closer = $('.closer'),
+      start = $('#startBtn'),
+      started = 0;
+
+
+createEntry.click(function() {
+  newEntry.show();
+});
+
+closer.click(function() {
+  newEntry.hide();
+});
+
+start.click(function() {
+  if (started == 0) {
+    start.html("Stop");
+    started = 1;
+  } else if (started == 1) {
+    start.html("Start");
+    started = 0;
+  }
+});
